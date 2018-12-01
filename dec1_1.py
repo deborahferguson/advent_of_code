221c1cc6fed00392b851d971299652f7a9555848
Debbie <dferguson41@gatech.edu>
2018-12-01 14:24:13 -0500

    Added comments

--- dec1_1.py
+++ dec1_1.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+#Load the data
 filename = "input_data/dec1_1_2.txt"
-
 changes = np.loadtxt(filename)
 
+#Sum all the changes
 resulting_frequency = np.sum(changes)
 
+#Output the solution as an int
 print(int(resulting_frequency))
--- dec1_2.py
+++ dec1_2.py
@@ -1,25 +1,30 @@
 import numpy as np
 
+#Load data
 filename = "input_data/dec1_1_2.txt"
 changes = np.loadtxt(filename)
 
-print(changes.shape)
-
+#Dict of all the sums
 sums = {}
 
+#Tracking the sum and whether or not we've found a repeat
 total = 0
 repeated = False
 
+#Storing the final answer
 answer = 0
 
+#Keep looping until we find a repeat
 while not repeated:
     for i in range(len(changes)):
+        #Calculate the new sum
         total=total+int(changes[i])
-        if(total in sums):
+        if(total in sums): #If the sum is a repeat
             answer = total
             repeated = True
             break
-        else:
+        else: #Otherwise add the sum to the dictionary
             sums[total] = 1
 
+#print the solution
 print(answer)
