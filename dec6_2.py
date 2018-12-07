1a71da57f7cf667f1690948eedf8e2136103d751
Debbie <dferguson41@gatech.edu>
2018-12-06 22:53:43 -0500

    Used pylint to improve coding style

--- dec6_2.py
+++ dec6_2.py
@@ -1,49 +1,48 @@
-filename = "input_data/dec6.txt"
-f = open(filename,'r')
-coordinates = []
-min_x = None
-max_x = None
-min_y = None
-max_y = None
-first = True
-for line in f:
-    values = line.split(', ')
-    x = int(values[0])
-    y = int(values[1])
-    coordinates.append((x,y))
-    if(first):
-        min_x = x
-        max_x = x
-        min_y = y
-        max_y = y
-        first = False
-    else:
-        if(x<min_x):
-            min_x = x
-        if(x>max_x):
-            max_x = x
-        if(y<min_y):
-            min_y = y
-        if(y>max_y):
-            max_y = y
-f.close()
+""" This solves the advent of code December 6 part 2"""
 
+def closest_region():
+    """This function finds the size of the region containing all locations
+    with a total distance to all coordinates that is less than 10000 """
+    input_file = open("input_data/dec6.txt", 'r')
+    coordinates = []
+    min_x = None
+    max_x = None
+    min_y = None
+    max_y = None
+    first = True
+    for line in input_file:
+        values = line.split(', ')
+        x_value = int(values[0])
+        y_value = int(values[1])
+        coordinates.append((x_value, y_value))
+        if first:
+            min_x = x_value
+            max_x = x_value
+            min_y = y_value
+            max_y = y_value
+            first = False
+        else:
+            if x_value < min_x:
+                min_x = x_value
+            if x_value > max_x:
+                max_x = x_value
+            if y_value < min_y:
+                min_y = y_value
+            if y_value > max_y:
+                max_y = y_value
+    input_file.close()
 
-count = 0
-#grid = []
-for i in range(max_x-min_x):
-    column = []
-    for j in range(max_y-min_y):
-        tot_dist = 0
-        x_loc = i+min_x
-        y_loc = j+min_y
-        for k in range(len(coordinates)):
-            distance = abs(coordinates[k][0]-x_loc)+abs(coordinates[k][1]-y_loc)
-            tot_dist += distance
-        if(tot_dist<10000):
-            count+=1
-#        column.append(tot_dist)
-#    grid.append(column)
+    count = 0
+    for i in range(max_x-min_x):
+        for j in range(max_y-min_y):
+            tot_dist = 0
+            x_loc = i+min_x
+            y_loc = j+min_y
+            for _, item in enumerate(coordinates):
+                distance = abs(item[0]-x_loc)+abs(item[1]-y_loc)
+                tot_dist += distance
+            if tot_dist < 10000:
+                count += 1
+    return count
 
-print(count)
-
+print(closest_region())
