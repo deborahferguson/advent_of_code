37e2681a1554420d5252c7669d3fef013520a580
Debbie <dferguson41@gatech.edu>
2018-12-06 23:07:56 -0500

    Obtained a 10/10 on pylint for dec6_2.py

--- dec6_2.py
+++ dec6_2.py
@@ -1,8 +1,8 @@
 """ This solves the advent of code December 6 part 2"""
 
-def closest_region():
-    """This function finds the size of the region containing all locations
-    with a total distance to all coordinates that is less than 10000 """
+def read_data():
+    """This function reads in the data and returns a list of coordinates
+    as tuples, the x range, and the y range"""
     input_file = open("input_data/dec6.txt", 'r')
     coordinates = []
     min_x = None
@@ -31,13 +31,18 @@ def closest_region():
             if y_value > max_y:
                 max_y = y_value
     input_file.close()
+    return coordinates, (min_x, max_y), (min_y, max_y)
 
+def closest_region():
+    """This function finds and returns the size of the region containing all locations
+    with a total distance to all coordinates that is less than 10000 """
+    coordinates, x_range, y_range = read_data()
     count = 0
-    for i in range(max_x-min_x):
-        for j in range(max_y-min_y):
+    for i in range(x_range[1]-x_range[0]):
+        for j in range(y_range[1]-y_range[0]):
             tot_dist = 0
-            x_loc = i+min_x
-            y_loc = j+min_y
+            x_loc = i+x_range[0]
+            y_loc = j+y_range[0]
             for _, item in enumerate(coordinates):
                 distance = abs(item[0]-x_loc)+abs(item[1]-y_loc)
                 tot_dist += distance
